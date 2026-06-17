6f0e1aef3af29b8c9411a4d38d74d273aafa6f53
Anthodido <anthony.didomenico678@gmail.com>
2026-06-17 11:15:42 +0200

    pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
+"""Module for measuring runtime of parallel coroutines."""
+import asyncio
+import time
 
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measures total runtime of 4 parallel async_comprehension calls."""
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for v in range(4)))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time
+#!/usr/bin/env python3
+"""Module for measuring runtime of parallel coroutines."""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -8,7 +23,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measures total runtime of 4 parallel async_comprehension calls."""
     start_time = time.time()
-    total = await asyncio.gather(*(async_comprehension()for v in range (4)))
+    await asyncio.gather(*(async_comprehension() for v in range(4)))
     end_time = time.time()
-    total_time =  end_time - start_time
+    total_time = end_time - start_time
     return total_time
