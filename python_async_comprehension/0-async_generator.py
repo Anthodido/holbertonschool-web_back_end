ee1ce19d70c86c4cc3c1ed965182fcb12350048d
Anthodido <anthony.didomenico678@gmail.com>
2026-06-17 11:11:08 +0200

    P1

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import random
+import asyncio
+
+
+async def async_generator() -> int:
+    """Async generator that yields numbers."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
